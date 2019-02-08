b075b26edaecd28e2e40a0e9418e057a46f86658
Niclas Holmquist Jensen <nicl2445@edu.eal.dk>
2019-02-08 08:37:33 +0100

    Add project files.

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a7bf829-d7ce-48ec-9f21-1e08eb35f1c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DatoConsoleSimpleAfprovning;
+using DateClassLibrary;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Year()
+        {
+            UclDate aDate = new UclDate(2019, 2, 6);
+            Assert.AreEqual(2019, aDate.GetYear());
+        }
+        [TestMethod]
+        public void Test_Month()
+        {
+            UclDate aDate = new UclDate(2019, 2, 6);
+            Assert.AreEqual(2, aDate.GetMonth());
+        }
+        [TestMethod]
+        public void Test_Day()
+        {
+            UclDate aDate = new UclDate(2019, 2, 6);
+            Assert.AreEqual(6, aDate.GetDay());
+        }
+        [TestMethod]
+        public void Test_set()
+        {
+            UclDate aDate = new UclDate(2019, 2, 6);
+            aDate.SetDay(92);
+            Assert.AreEqual(92, aDate.GetDay());
+        }
+        [TestMethod]
+        public void Test_setMonth()
+        {
+            UclDate aDate = new UclDate(2019, 2, 6);
+            aDate.SetMonth(15);
+            Assert.AreEqual(15, aDate.GetMonth());
+        }
+        [TestMethod]
+        public void Test_setDay()
+        {
+            UclDate aDate = new UclDate(2019, 2, 6);
+            aDate.SetYear(2015);
+            Assert.AreEqual(2015, aDate.GetYear());
+        }
+        [TestMethod]
+        public void Test_format()
+        {
+            UclDate aDate = new UclDate(2019, 4, 600000000);
+            Assert.AreEqual("2019,2,6", aDate.GetDatoStringYMD());
+        }
+        [TestMethod]
+        public void Test_ugedag()
+        {
+            UclDate aDate = new UclDate(2019, 15, 90945);
+            Assert.AreEqual(1, aDate.GetWeekDay());
+        }
+    }
+}
